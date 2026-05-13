bb7975e3879be986718c97f840a0be655b988094
Rafus27 <s.rafael2704@gmail.com>
2026-05-13 14:40:01 +0400

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,290 @@
+const WebSocket = require("ws");
+
+const games = new Map();
+const keys = new Map();
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        let m = JSON.parse(message);
+        let game;
+
+
+        switch (m.type) {
+            case "CREATE":
+                game = new Game();
+                games.set(game.id, game);
+                keys.set(game.get_key(), game.id);
+                game.add_listener(ws);
+                ws.send(resp("KEY", { key: game.get_key(), id: game.id }));
+                console.log(`game ${game.id} started`);
+                break;
+
+            case "JOIN":
+
+                // let player = new Player(m.data.player.id, m.data.player.name, m.data.player.color, m.data.player.symbol);
+
+                if (keys.has(m.data.key)) {
+                    games.get(keys.get(m.data.key)).add_listener(ws);
+                    ws.send(resp("INFO", games.get(keys.get(m.data.key)).add_player(m.data.player)));
+                    ws.send(resp("JOIN_DATA", { id: games.get(keys.get(m.data.key)).id, key: games.get(keys.get(m.data.key)).get_key() }))
+                    games.get(keys.get(m.data.key)).broadcast();
+                    console.log(`player ${m.data.player.id} added`);
+                } else {
+                    ws.send(resp("ERROR", "Key isn't exist"));
+                    console.log("error: add player");
+                }
+                break;
+
+            case "RECONNECT":
+                if (games.has(m.data.game_id)) {
+                    game = games.get(m.data.game_id);
+                    game.players.forEach((item, i) => {
+                        if (item.id === m.data.player_id) {
+                            game.add_listener(ws);
+                            ws.send(resp("PLAYER_DATA", game.players[i]));
+                            game.broadcast();
+                        }
+                        // ws.send(resp("ERROR", "Player isn't exist"));
+                    })
+                } else {
+                    ws.send(resp("ERROR", "Game isn't exist"));
+                }
+                break;
+
+            case "MOVE":
+                if (!games.has(m.data.game_id)) {
+                    ws.send(resp("ERROR", "Game isn't exist"));
+                    console.log("error: move: game not exist");
+                    break;
+                }
+                game = games.get(m.data.game_id);
+                let player;
+                game.players.forEach((item, i) => {
+                    if (item.id === m.data.player_id) {
+                        player = game.players[i];
+                    }
+                })
+                if (player === undefined) {
+                    ws.send(resp("ERROR", "Player isn't exist"));
+                    onsole.log("error: move: player not exist");
+                    break;
+                }
+                game.move(player);
+                game.broadcast();
+                break;
+
+            default:
+                break;
+
+        }
+    })
+});
+
+console.log("Server started");
+
+
+function resp(type, data) {
+    return JSON.stringify({ type: type, data: data });
+}
+
+
+class Player {
+
+    pos = {
+        cur: 0,
+        wrp: 0,
+        prw: 0
+    };
+    stat = {
+        last_nsix: 0,
+        six_count: 0,
+        one_count: 0,
+    }
+    dice = 0;
+
+    constructor(/*id,*/ name, color, symbol) {
+        //this.id = id
+        this.name = name;
+        this.color = color;
+        this.symbol = symbol;
+    };
+}
+
+class Game {
+
+    id = crypto.randomUUID();
+    step = 0;
+    turn = 0;
+    players = [];
+    create_time = Date.now();
+
+    // Network
+
+    add_listener(ws) {
+        ws.game_id = this.id;
+    };
+
+    broadcast() {
+        wss.clients.forEach((client) => {
+            if (client.game_id === this.id && client.readyState === 1) {
+                client.send(this.form_response("GAME_DATA", { turn: this.players[this.turn].id, players: this.players }));
+            }
+        })
+    };
+
+    form_response(type, data) {
+        return JSON.stringify({ type: type, data: data })
+    }
+
+    // Players
+
+    add_player(player) {
+
+        this.players.forEach((item) => {
+            if (item.id === player.id) return "Already Exist";
+        })
+        this.players.push(player);
+        return "Player Added";
+    };
+
+    delete_player(player_id) {
+        this.players.forEach((item, index) => {
+            if (item.id === player_id) {
+                this.players.splice(index, 1);
+                return "Player Deleted";
+            }
+            return "Not Exist"
+        })
+    };
+
+    get_key() {
+        return this.id.split("-")[1].toUpperCase();
+    };
+
+    // Game
+
+    move(player) {
+
+        if (this.players[this.turn] !== player) {
+            console.log("not your turn");
+
+            return "Not Your Turn";
+        }
+
+        player.pos.prw = player.pos.cur;
+        let pos = player.pos.cur;
+        let add = this.roll_dice();
+        player.dice = add;
+
+        if (pos == 0 && add == 6) {
+            add = 0;
+            pos = 1;
+            player.stat.last_nsix = 1;
+        } else if (pos == 0) {
+            add = 0;
+        }
+
+        if (pos + add > 72) {
+            pos = 72;
+            add = 0;
+        }
+
+        if (add == 6 && player.stat.six_count >= 2) {
+            player.stat.six_count = 0;
+            pos = player.stat.last_nsix;
+            add = 0;
+        } else if (add == 6) {
+            player.stat.six_count++;
+        } else {
+            player.stat.last_nsix = pos;
+            player.stat.six_count = 0;
+        }
+
+        if (pos > 68 && pos < 72) {
+            if (add > 3) add = 0;
+
+            if (add == 1 && player.stat.one_count >= 2) {
+                pos = 51;
+                add = 0;
+                player.stat.one_count = 0
+            } else if (add == 1) {
+                player.stat.one_count++;
+            }
+        }
+
+        player.pos.wrp = pos + add;
+
+        player.pos.cur = this.warp(pos + add);
+
+        this.players[this.turn] = player
+
+        this.set_turn();
+
+        console.log(`player ${player.id} moved on position ${player.pos.cur}`);
+
+        return "Player Moved";
+    };
+
+    set_turn(t = -1) {
+        const len = this.players.length;
+
+        if (t == -1) {
+            this.turn = (this.turn + 1) % len;
+            return "Turn Increased";
+        } else if (t < len && t >= 0) {
+            this.turn = t;
+            return `Turn Set on ${t}`;
+        } else {
+            return "Error Turn";
+        }
+    };
+
+    roll_dice(dice = 0) {
+        if (dice === 0) {
+            let a = new Uint32Array(1);
+            crypto.getRandomValues(a);
+            return 1 + (a[0] % 6);
+        } else {
+            return dice;
+        }
+    }
+
+    warp(pos) {
+        const warp = new Map([
+            [10, 23],
+            [16, 4],
+            [20, 32],
+            [24, 7],
+            [28, 50],
+            [37, 66],
+            [45, 67],
+            [52, 35],
+            [55, 3],
+            [63, 2],
+            [12, 8],
+            [17, 69],
+            [22, 60],
+            [27, 41],
+            [29, 6],
+            [44, 9],
+            [46, 62],
+            [54, 68],
+            [61, 13],
+            [72, 51]
+        ]);
+
+        if (warp.has(pos)) {
+            return warp.get(pos);
+        } else {
+            return pos;
+        }
+    }
+
+
+
+
+
+
+}
---
+++ nginx/html/script.js
@@ -0,0 +1,222 @@
+// CLASSES
+
+class Network {
+    constructor(url) {
+        this.socket = null;
+        this.url = `ws://${url}/api/`;
+    }
+
+    connect() {
+        this.socket = new WebSocket(this.url);
+
+        // this.socket.onopen = () => console.log("connected");
+
+        this.socket.onclose = () => {
+            console.log("Disconnected");
+            setTimeout(() => this.connect(), 3000);
+        }
+
+        this.socket.onmessage = (e) => {
+            this.handle(JSON.parse(e.data));
+        }
+    }
+
+    send(type, data) {
+        if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+            this.socket.send(JSON.stringify({ type: type, data: data }));
+        } else {
+            console.error("WebSocket Closed");
+        }
+    }
+
+    handle(message) {
+
+        switch (message.type) {
+            case "KEY":
+                state.key = message.data.key;
+                state.id = message.data.id;
+                console.log(state.key);
+                break;
+            case "ERROR" || "INFO":
+                console.log(message.data);
+                break;
+            case "GAME_DATA":
+                state.turn = message.data.turn;
+                state.players = message.data.players;
+                if (state.turn === current_player.id) {
+                    document.getElementById("roll").classList.add("active");
+                } else {
+                    document.getElementById("roll").classList.remove("active");
+                }
+                state.players.forEach((item) => {
+                    display_player(item)
+                });
+                console.log(state.players[0].pos);
+
+                break;
+            case "JOIN_DATA":
+                state.id = message.data.id;
+                state.key = message.data.key;
+                localStorage.setItem("current_game_id", state.id);
+                localStorage.setItem("current_player_id", current_player.id);
+                console.log(state);
+                break;
+            case "PLAYER_DATA":
+                current_player = message.data;
+                console.log(current_player);
+                break;
+            default:
+                break;
+        }
+    };
+}
+
+class Player {
+
+    id = crypto.randomUUID();
+    pos = {
+        cur: 0,
+        wrp: 0,
+        prw: 0
+    };
+    stat = {
+        last_nsix: 0,
+        six_count: 0,
+        one_count: 0,
+    }
+    dice = 0;
+
+    constructor(name, color, symbol) {
+        this.name = name;
+        this.color = color;
+        this.symbol = symbol;
+    };
+}
+
+
+// MAIN
+
+const url = "127.0.0.1:8080";
+
+const socket = new Network(url);
+
+socket.connect();
+
+let current_player = {}
+
+let state = {
+    id: "",
+    key: "",
+    turn: "",
+    players: []
+}
+
+field_generation();
+
+document.getElementById("single").addEventListener("click", (e) => { main(false, true) });
+
+document.getElementById("join").addEventListener("click", (e) => { main(true, false) });
+
+document.getElementById("reconnect").addEventListener("click", (e) => { reconnect_game() });
+
+document.getElementById("admin").addEventListener("click", (e) => { main(true, true) });
+
+document.getElementById("roll").addEventListener("click", (e) => { make_move() });
+
+
+
+// FUNCTIONS
+
+function main(is_mp, is_adm) {
+
+    if (is_mp && is_adm) {
+        socket.send("CREATE");
+    } else if (is_mp && !is_adm) {
+        let p_name = document.getElementById("name").value;
+        let p_color = document.getElementById("color").value;
+        let p_symbol = "_";
+        let p_key = document.getElementById("key").value;
+
+        p_key = p_key.toUpperCase();
+
+        current_player = new Player(p_name, p_color, p_symbol);
+
+        socket.send("JOIN", { key: p_key, player: current_player });
+    }
+}
+
+function field_generation() {
+    let t = document.getElementById("field");
+    let a = [];
+    let html = `<table class="field-table"><tr>`;
+
+    for (let i = 0; i < 8; i++) {
+        p = i * 9;
+        n = 72 - i * 9;
+
+        if (i % 2 == 0) {
+            for (let j = 1; j <= 9; j++) {
+                a[p] = n;
+                p++;
+                n--;
+            }
+        } else {
+            p += 8;
+            for (let j = 1; j <= 9; j++) {
+                a[p] = n;
+                p--;
+                n--;
+            }
+        }
+    };
+
+    a.forEach((item, i) => {
+        html += `<td><div class="cont"><div class="num"></div><div id="${item}" class="cell"></div></div></td>`;
+        if ((i + 1) % 9 === 0 && i !== a.length - 1) {
+            html += `</tr><tr>`;
+        }
+    });
+
+    html += `</tr></table>`;
+
+    t.innerHTML = html;
+
+    t.addEventListener("click", (e) => {
+        console.log(e.target); // REWORK!!!
+    });
+}
+
+function display_player(player) {
+    let html = `<div id="${player.id}" class="player"></div>`;
+
+    const a = document.getElementById(player.id);
+    if (a) { a.remove() };
+
+    const b = document.getElementById(`${player.pos.cur}`);
+    if (b) { b.innerHTML += html };
+}
+
+function join_game() {
+    let key = document.getElementById("key").value;
+    let name = document.getElementById("name").value;
+    let color = document.getElementById("color").value;
+
+    current_player = new Player(name, color, "_");
+
+    console.log(current_player);
+
+    socket.send("JOIN", { key: key, player: current_player });
+}
+
+function make_move() {
+    if (state.turn === current_player.id) {
+        socket.send("MOVE", { game_id: state.id, player_id: current_player.id });
+    }
+}
+
+function reconnect_game(game_id = localStorage.getItem("current_game_id"), player_id = localStorage.getItem("current_player_id")) {
+    state.id = game_id;
+    socket.send("RECONNECT", { game_id, player_id });
+}
+
+
