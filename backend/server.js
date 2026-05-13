25134ef4ade372a93f9eb460f4d76f646e297493
Rafus27 <75754105+Rafus27@users.noreply.github.com>
2026-05-13 18:07:08 +0400

    nginx test mode

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const WebSocket = require("ws");
 const games = new Map();
 const keys = new Map();
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 3000 });
 
 wss.on("connection", (ws) => {
     ws.on("message", (message) => {
